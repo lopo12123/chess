3daa28b63001c8afd5ab934c859342bfbd48f3fc
lopo <lopo@zju.edu.cn>
2022-04-24 10:15:28 +0800

    chore: container

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,31 @@
 <template>
     <div class="app">
-        app root
-        <router-view/>
+        <div class="banner">
+            a banner, but no thing to show.
+        </div>
+        <div class="main">
+            <router-view/>
+        </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+.app {
+    position: relative;
+    width: 100%;
+    height: 100%;
+
+    .banner {
+        position: relative;
+        width: 100%;
+        height: 60px;
+        border-bottom: solid 1px #cccccc;
+    }
+
+    .main {
+        position: relative;
+        width: 100%;
+        height: calc(100% - 61px);
+    }
+}
+</style>
