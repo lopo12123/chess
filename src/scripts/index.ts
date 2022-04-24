72e721dcbc10c6fdc9164dff347cf395204bc748
lopo <lopo@zju.edu.cn>
2022-04-24 14:01:30 +0800

    feat: jump and copy

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -5,21 +5,23 @@ const enum State {
 }
 
 class ChessBoard {
+    readonly #size: number
     readonly #board: State[][]  // y x
 
-    private assimilate(to: State, pos: [ number, number ]) {
-        for (let i = pos[1] - 1; i <= pos[1] + 1; i++) {
-            for (let j = pos[0] - 1; j <= pos[0] + 1; j++) {
+    private doAssimilate(state: State, center: [ number, number ]) {
+        for (let i = Math.max(center[1] - 1, 0); i <= Math.min(center[1] + 1, this.#size - 1); i++) {
+            for (let j = Math.max(center[0] - 1, 0); j <= Math.min(center[0] + 1, this.#size - 1); j++) {
                 if(this.#board[i][j] !== undefined
                     && this.#board[i][j] !== State.empty
-                    && this.#board[i][j] !== to) {
-                    this.#board[i][j] = to
+                    && this.#board[i][j] !== state) {
+                    this.#board[i][j] = state
                 }
             }
         }
     }
 
     constructor(size: number = 9) {
+        this.#size = size
         this.#board = []
         for (let y = 0; y < size; y++) {
             this.#board[y] = []
@@ -29,14 +31,24 @@ class ChessBoard {
         }
     }
 
-    place(state: Exclude<State, State.empty>, positions: [ number, number ][]) {
+    /**
+     * @description 在一些点位放置一些棋子 (无同化)
+     */
+    doPlace(state: Exclude<State, State.empty>, positions: [ number, number ][]) {
         positions.forEach((point) => {
             this.#board[point[1]][point[0]] = state
         })
     }
 
-    go(user: State) {
+    doJump(state: Exclude<State, State.empty>, from: [ number, number ], to: [ number, number ]) {
+        this.#board[from[1]][from[0]] = State.empty
+        this.#board[to[1]][to[0]] = state
+        this.doAssimilate(state, to)
+    }
 
+    doCopy(state: Exclude<State, State.empty>, to: [ number, number ]) {
+        this.#board[to[1]][to[0]] = state
+        this.doAssimilate(state, to)
     }
 
     see() {
