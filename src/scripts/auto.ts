a8484e1cfcb49b68ff285444fe3672b0498d8b06
lopo <lopo@zju.edu.cn>
2022-04-26 15:37:39 +0800

    refactor: 抽离非必要的类方法

--- src/scripts/auto.ts
+++ src/scripts/auto.ts
@@ -1,5 +1,37 @@
 import { getOpponent, State } from "./index";
 
+/**
+ * @description 找出目标点位周围的所有空位
+ */
+const findEmptyAround = (board: State[][], pos: [ x: number, y: number ])
+    : { copy: [ number, number ][], jump: [ number, number ][] } => {
+    const res: { copy: [ number, number ][], jump: [ number, number ][] } = { copy: [], jump: [] }
+    for (let y = Math.max(pos[1] - 2, 0); y <= Math.min(pos[1] + 2, board.length - 1); y++) {
+        for (let x = Math.max(pos[0] - 2, 0); x <= Math.min(pos[0] + 2, board.length - 1); x++) {
+            if(x === pos[0] && y === pos[1]) continue
+            if(board[y][x] === State.empty) {
+                (Math.abs(x - pos[0]) <= 1 && Math.abs(y - pos[1]) <= 1)
+                    ? (res.copy.push([ x, y ]))
+                    : (res.jump.push([ x, y ]))
+            }
+        }
+    }
+    return res
+}
+
+/**
+ * @description 统计落子在pos能同化的数量
+ */
+const countAssimilate = (board: State[][], pos: [ x: number, y: number ], self: Exclude<State, State.empty>): number => {
+    let count = 0
+    for (let y = Math.max(pos[1] - 2, 0); y <= Math.min(pos[1] + 2, board.length - 1); y++) {
+        for (let x = Math.max(pos[0] - 2, 0); x <= Math.min(pos[0] + 2, board.length - 1); x++) {
+            if(board[y][x] === getOpponent(self)) count++
+        }
+    }
+    return count
+}
+
 class Auto {
     readonly #size: number
 
@@ -23,38 +55,6 @@ class Auto {
     }
 
     /**
-     * @description 统计落子在pos能同化的数量
-     */
-    private countAssimilate(board: State[][], pos: [ x: number, y: number ], self: Exclude<State, State.empty>): number {
-        let count = 0
-        for (let y = Math.max(pos[1] - 2, 0); y <= Math.min(pos[1] + 2, this.#size - 1); y++) {
-            for (let x = Math.max(pos[0] - 2, 0); x <= Math.min(pos[0] + 2, this.#size - 1); x++) {
-                if(board[y][x] === getOpponent(self)) count++
-            }
-        }
-        return count
-    }
-
-    /**
-     * @description 找出目标点位周围的所有空位
-     */
-    private findEmptyAround(board: State[][], pos: [ x: number, y: number ])
-        : { copy: [ number, number ][], jump: [ number, number ][] } {
-        const res: { copy: [ number, number ][], jump: [ number, number ][] } = { copy: [], jump: [] }
-        for (let y = Math.max(pos[1] - 2, 0); y <= Math.min(pos[1] + 2, this.#size - 1); y++) {
-            for (let x = Math.max(pos[0] - 2, 0); x <= Math.min(pos[0] + 2, this.#size - 1); x++) {
-                if(x === pos[0] && y === pos[1]) continue
-                if(board[y][x] === State.empty) {
-                    (Math.abs(x - pos[0]) <= 1 && Math.abs(y - pos[1]) <= 1)
-                        ? (res.copy.push([ x, y ]))
-                        : (res.jump.push([ x, y ]))
-                }
-            }
-        }
-        return res
-    }
-
-    /**
      * @description 分析当前状态并得出得分最高的选择
      */
     doAnalyse(board: State[][], self: Exclude<State, State.empty>)
@@ -75,11 +75,11 @@ class Auto {
             for (let x = 0; x < this.#size; x++) {
                 if(board[y][x] === self) {
                     // 统计周围的空位
-                    const emptyAround = this.findEmptyAround(board, [ x, y ])
+                    const emptyAround = findEmptyAround(board, [ x, y ])
 
                     // 统计空位复制的得分并选取最优
                     emptyAround.copy.forEach((copyPoint) => {
-                        const copyScore = this.countAssimilate(board, copyPoint, self) * 2 + 1
+                        const copyScore = countAssimilate(board, copyPoint, self) + 1
                         if(copyScore > bestTillNow.score) {
                             bestTillNow.score = copyScore
                             bestTillNow.solutions = [ {
@@ -99,7 +99,7 @@ class Auto {
 
                     // 统计空位跳跃的得分并取最优
                     emptyAround.jump.forEach((jumpPoint) => {
-                        const jumpScore = this.countAssimilate(board, jumpPoint, self) * 2
+                        const jumpScore = countAssimilate(board, jumpPoint, self)
                         if(jumpScore > bestTillNow.score) {
                             bestTillNow.score = jumpScore
                             bestTillNow.solutions = [ {
