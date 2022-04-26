3e0a6b6dbc0ea0aaaf2371e0b8cd57deaff56849
lopo <lopo@zju.edu.cn>
2022-04-26 14:57:49 +0800

    feat: ai提示和自动执行

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
 import { ref } from "vue";
 import { ChessBoard, CountInfo, Levels, State } from "@/scripts";
+import { Auto } from "@/scripts/auto";
 
 // 初始化
 const game = new ChessBoard()
+const ai = new Auto()
 
 // region 状态展示及结束判断
 const initState = game.see()
@@ -11,7 +13,11 @@ const board = ref<State[][]>(initState[0])
 const count = ref<CountInfo>(initState[1])
 const judgeIfEnd = () => {
     const [ ifEnd, winner ] = game.doJudge()
-    if(ifEnd) alert(`game is over, winner is [${ winner }]`)
+    if(ifEnd) {
+        // 提示并加锁
+        alert(`game is over, winner is [${ winner }]`)
+        selectLevel()
+    }
 }
 // endregion
 
@@ -47,8 +53,27 @@ const clickBoard = (x: number, y: number) => {
         // 更新状态
         ;[ board.value, count.value ] = game.see()
         judgeIfEnd()
+        aiState.value = 'hide'
     }
 }
+
+// region ai
+const aiState = ref<'show' | 'hide'>('hide')
+const aiThink = ref<{ from: [ number, number ], to: [ number, number ], type: 'copy' | 'jump' }[]>([])
+const askAi = () => {
+    aiThink.value = ai.doAnalyse(board.value, currentPlayer.value).solutions
+    aiState.value = 'show'
+}
+const letAi = () => {
+    // 分析
+    const best = ai.randomPick(ai.doAnalyse(board.value, currentPlayer.value).solutions)
+    // 拿起
+    clickBoard(...best.from)
+    // 放下
+    clickBoard(...best.to)
+    aiState.value = 'hide'
+}
+// endregion
 
 // region 开始
 const level = ref(0)
@@ -57,11 +82,7 @@ const selectLevel = () => {
         alert('超出范围')
         return
     }
-    // game.doInit(Levels[level.value])
-    game.doInit({
-        black: [[0,0]],
-        white: [[1,1], [2,2]]
-    })
+    game.doInit(Levels[level.value])
     userState.value = 'toSelect'
     currentPlayer.value = State.black
     selected.value = [ -100, -100 ]
@@ -89,6 +110,32 @@ const ifSelf = (x: number, y: number) => {
 <template>
     <div class="home">
         <div class="brief-container">
+            <!-- region AI -->
+            <div class="line">AI:</div>
+            <div class="line">
+                <button @click="askAi">提示</button>
+                <button @click="letAi">执行</button>
+            </div>
+            <div class="ai-thinks" v-if="aiState==='show'">
+                <div class="line" v-for="(solution, index) in aiThink" :key="'solution'+index">
+                    <span :class="'hl-' + solution.type" style="margin-right: 5px;color: #777777">
+                        {{ solution.type }}
+                    </span>
+                    (<span style="color: orange">{{ solution.from[0] }}</span>,
+                    <span style="color: green">{{ solution.from[1] }}</span>)
+                    to
+                    (<span style="color: orange">{{ solution.to[0] }}</span>,
+                    <span style="color: green">{{ solution.to[1] }}</span>)
+                </div>
+            </div>
+            <!-- endregion -->
+
+            <!-- region 指示当前 -->
+            <div class="line">当前</div>
+            <div class="line"><i :class="['label-icon', 'ceil_'+currentPlayer]"/></div>
+            <!-- endregion -->
+
+            <!-- region 选择 -->
             <div class="line">选择:</div>
             <div class="line">
                 <select class="level-selector" v-model="level" @change="selectLevel">
@@ -96,20 +143,31 @@ const ifSelf = (x: number, y: number) => {
                     <option value="1">lv.2</option>
                 </select>
             </div>
-            <div class="line">当前</div>
-            <div class="line"><i :class="['label-icon', 'ceil_'+currentPlayer]"/></div>
+            <!-- endregion -->
+
+            <!-- region 统计 -->
             <div class="line">统计:</div>
             <div class="line"><i class="label-icon ceil_black"/>{{ count.black }}</div>
             <div class="line"><i class="label-icon ceil_white"/>{{ count.white }}</div>
             <div class="line"><i class="label-icon ceil"/>{{ count.empty }}</div>
+            <!-- endregion -->
+
+            <!-- region 图例 -->
             <div class="line">图例:</div>
             <div class="line"><i class="label-icon ceil hl-self"/>选择/取消选择</div>
             <div class="line"><i class="label-icon ceil hl-copy"/>复制</div>
             <div class="line"><i class="label-icon ceil hl-jump"/>跳跃</div>
+            <!-- endregion -->
         </div>
         <div class="board-container">
-            <div class="line"
-                 v-for="(line, line_index) in board" :key="'line'+line_index">
+            <div class="line">
+                <div class="ceil_ceil"></div>
+                <div class="ceil_ceil" v-for="(drop, ceil_index) in board.length" :key="ceil_index">
+                    {{ ceil_index }}
+                </div>
+            </div>
+            <div class="line" v-for="(line, line_index) in board" :key="'line'+line_index">
+                <div class="ceil_line">{{ line_index }}</div>
                 <div v-for="(ceil, ceil_index) in line" :key="'column'+ceil_index"
                      :class="{
                         ['ceil_'+ceil]: true,
@@ -127,6 +185,8 @@ const ifSelf = (x: number, y: number) => {
 </template>
 
 <style lang="scss" scoped>
+@use "../styles/mixin";
+
 .home {
     position: relative;
     width: 100%;
@@ -139,6 +199,14 @@ const ifSelf = (x: number, y: number) => {
         position: relative;
         width: 200px;
         height: 100%;
+
+        .ai-thinks {
+            @include mixin.scrollBarStyle();
+            position: relative;
+            width: 100%;
+            max-height: 300px;
+            overflow: hidden auto;
+        }
 
         .line {
             position: relative;
@@ -195,6 +263,20 @@ const ifSelf = (x: number, y: number) => {
         @extend %ceil-base;
     }
 
+    .ceil_line {
+        @extend %ceil-base;
+        border-color: transparent;
+        color: green;
+        font-size: 12px;
+    }
+
+    .ceil_ceil {
+        @extend %ceil-base;
+        border-color: transparent;
+        color: orange;
+        font-size: 12px;
+    }
+
     .ceil_empty {
         @extend %ceil-base;
     }
@@ -213,8 +295,8 @@ const ifSelf = (x: number, y: number) => {
             display: inline-block;
         }
 
-        &:hover::before {
-            background-color: brown;
+        &:hover {
+            background-color: darkseagreen;
         }
     }
 
@@ -231,6 +313,10 @@ const ifSelf = (x: number, y: number) => {
             background-color: #777777;
             display: inline-block;
         }
+
+        &:hover {
+            background-color: darkseagreen;
+        }
     }
 
     .hl-self {
@@ -241,6 +327,7 @@ const ifSelf = (x: number, y: number) => {
         background-color: #00800066 !important;
         cursor: pointer;
     }
+
     .hl-jump__disabled {
         background-color: #00800033 !important;
         cursor: pointer;
@@ -250,6 +337,7 @@ const ifSelf = (x: number, y: number) => {
         background-color: #ffa50066 !important;
         cursor: pointer;
     }
+
     .hl-copy__disabled {
         background-color: #ffa50033 !important;
         cursor: pointer;
